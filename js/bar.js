e5403a828f3fb68074bdfdf6488d4ae0fde5784e
samina333 <saminakhan54688@gmail.com>
2020-07-30 21:52:57 +0500

    update

---
+++ js/bar.js
@@ -0,0 +1,441 @@
+new Chart(document.getElementById("bar"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [22, 33, 55, 32, 86, 23, 14],
+      "fill": false,
+      "backgroundColor": "#00c851",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> b92bb6d6f2c0d12041a1d6de3386cd6c5494dc89
+new Chart(document.getElementById("bar88"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [22, 33, 55, 32, 86, 23, 14],
+      "fill": false,
+      "backgroundColor": "#00c851",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> e54d1199679c8884de012817fb7301bc211c4666
+>>>>>>> b92bb6d6f2c0d12041a1d6de3386cd6c5494dc89
+
+new Chart(document.getElementById("bar1"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [20, 33, 45, 34, 71, 85, 54],
+      "fill": false,
+      "backgroundColor": "#fb3",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+
+new Chart(document.getElementById("bar2"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [35, 21, 42, 59, 50, 62, 90],
+      "fill": false,
+      "backgroundColor": "#ff3547",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+
+new Chart(document.getElementById("bar3"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [22, 33, 55, 32, 86, 23, 14],
+      "fill": false,
+      "backgroundColor": "#33b5e5",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+
+new Chart(document.getElementById("bar4"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [20, 33, 45, 34, 71, 54, 44],
+      "fill": false,
+      "backgroundColor": "#fb3",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+
+new Chart(document.getElementById("bar5"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [60, 21, 72, 65, 50, 40, 35],
+      "fill": false,
+      "backgroundColor": "#ff3547",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+
+new Chart(document.getElementById("bar6"), {
+  "type": "bar",
+  "data": {
+    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+    "datasets": [{
+      "label": "My First Dataset",
+      "data": [21, 59, 90, 35, 50, 42, 62],
+      "fill": false,
+      "backgroundColor": "#00c851",
+      "borderWidth": 0
+    }]
+  },
+  "options": {
+    "responsive": false,
+    "legend": {
+      "display": false,
+    },
+    "scales": {
+      "xAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+          "beginAtZero": true
+        }
+      }],
+      "yAxes": [{
+        "gridLines": {
+          "display": false,
+          "drawBorder": false,
+        },
+        "ticks": {
+          "display": false,
+        }
+      }],
+    }
+  }
+});
+
+
+
+
+//line
+var ctxL = document.getElementById("lineChart1").getContext('2d');
+var myLineChart = new Chart(ctxL, {
+  type: 'line',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October"],
+    datasets: [{
+      pointBackgroundColor: '#fff',
+      backgroundColor: 'transparent',
+      borderColor: 'rgba(255, 255, 255)',
+      data: [2500, 2550, 5000, 3100, 7000, 5500, 4950, 16000, 10500, 8000],
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+          color: "transparent",
+          zeroLineColor: "transparent"
+        },
+        ticks: {
+          fontColor: "#fff",
+        },
+      }],
+      yAxes: [{
+        display: true,
+        gridLines: {
+          display: true,
+          drawBorder: false,
+          color: "rgba(255,255,255,.25)",
+          zeroLineColor: "rgba(255,255,255,.25)"
+        },
+        ticks: {
+          fontColor: "#fff",
+          beginAtZero: true,
+          stepSize: 5000
+        },
+      }],
+    }
+  }
+});
+
+// Minimalist charts
+$(function () {
+  $('.min-chart#chart-pageviews').easyPieChart({
+    barColor: "#3059B0",
+    onStep: function (from, to, percent) {
+      $(this.el).find('.percent').text(Math.round(percent));
+    }
+  });
+  $('.min-chart#chart-downloads').easyPieChart({
+    barColor: "#3059B0",
+    onStep: function (from, to, percent) {
+      $(this.el).find('.percent').text(Math.round(percent));
+    }
+  });
+  $('.min-chart#chart-sales').easyPieChart({
+    barColor: "#3059B0",
+    onStep: function (from, to, percent) {
+      $(this.el).find('.percent').text(Math.round(percent));
+    }
+  });
+});
+
+     var ctxL = document.getElementById("lineChart11").getContext('2d');
+    var gradientFill = ctxL.createLinearGradient(0, 0, 0, 290);
+    gradientFill.addColorStop(0, "rgba(0, 125, 250, 1)");
+    gradientFill.addColorStop(1, "rgba(0, 125, 250, 0.1)");
+    var myLineChart = new Chart(ctxL, {
+      type: 'line',
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+<<<<<<< HEAD
+            label: "Sales",
+=======
+<<<<<<< HEAD
+            label: "Sales",
+=======
+            label: "My First dataset",
+>>>>>>> e54d1199679c8884de012817fb7301bc211c4666
+>>>>>>> b92bb6d6f2c0d12041a1d6de3386cd6c5494dc89
+            data: [0, 65, 45, 65, 35, 65, 0],
+            backgroundColor: gradientFill,
+            borderColor: [
+              '#007DFA',
+            ],
+            borderWidth: 2,
+            pointBorderColor: "#007DFA",
+            pointBackgroundColor: "rgba(0, 125, 250, 1)",
+          }
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    });
